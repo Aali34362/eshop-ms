da5f97c5a4518308ca94007e40ab2293163065c1
Aamir Ali <aa882755@gmail.com>
2024-06-16 14:05:43 +0530

    GU

--- Packaged/BasePackage/BaseIntegrated/GlobalUsing.cs
+++ Packaged/BasePackage/BaseIntegrated/GlobalUsing.cs
@@ -1,10 +1,4 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Text;
-global using System.Threading.Tasks;
-
-global using Microsoft.EntityFrameworkCore;
+﻿global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Logging;
 global using Polly;
 global using MediatR;
