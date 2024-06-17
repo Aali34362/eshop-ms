7e4d15390e63a3a464b0ebc7ef59bcd46a94a49f
Aamir Ali <aa882755@gmail.com>
2024-06-17 20:47:28 +0530

    Adding Models and Creating new proto

---
+++ Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -0,0 +1,8 @@
+﻿namespace Discount.Grpc.Models;
+
+public class Coupon : BaseRequest
+{
+    public string ProductName { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public int Amount { get; set; } = 0;
+}
