c6c1a6db602b2951447ae81578d49c36f658df69
Aamir Ali <aa882755@gmail.com>
2024-06-21 07:21:29 +0530

    Models

--- Services/Ordering/Ordering.Domain/GlobalUsing.cs
+++ Services/Ordering/Ordering.Domain/GlobalUsing.cs
@@ -1,1 +1,2 @@
 ﻿global using MediatR;
+global using Ordering.Domain.Abstractions;
---
+++ Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.Domain.Models;
+
+public class OrderItem : Entity<Guid>
+{
+    internal OrderItem(Guid OrderId, Guid ProductId, int Quantity, decimal Price) 
+    {
+        this.OrderId = OrderId;
+        this.ProductId = ProductId;
+        this.Quantity = Quantity;
+        this.Price = Price;
+    }
+    public Guid OrderId { get; private set; } = default!;
+    public Guid ProductId { get; private set; } = default!;
+    public int Quantity { get; private set; } = default!;
+    public decimal Price { get; private set; } = default!;
+}
