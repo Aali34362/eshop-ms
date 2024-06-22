7a8dfcb250b12af380f48cf845da1675734754d2
Aamir Ali <aa882755@gmail.com>
2024-06-22 11:04:25 +0530

    Strongly Type ID

--- Services/Ordering/Ordering.Domain/ValueObjects/StronglyTypedId.cs
+++ Services/Ordering/Ordering.Domain/ValueObjects/StronglyTypedId.cs
@@ -1,22 +1,9 @@
 ﻿namespace Ordering.Domain.ValueObjects;
 
-public static class OfClass
-{
-    public static T Of<T>(Guid value, Func<Guid, T> factory)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-        if (value == Guid.Empty)
-            throw new DomainException($"{nameof(T)} cannot be empty");
-        return factory(value);
-    }
-}
-
 public record CustomerId
 {
     public Guid Value { get; set; }
     private CustomerId(Guid value) => Value = value;
-    //CustomerId customerId = OfClass.Of<CustomerId>(, guid => new CustomerId(guid));
-
     public static CustomerId Of(Guid value)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -27,19 +14,53 @@ public record CustomerId
 }
 public record OrderName
 {
+    private const int DefaultLength = 5;
     public string? Value { get; set; }
+    private OrderName(string value) => Value = value;
+    public static OrderName Of(string value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(value);
+        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength);
+        if (string.IsNullOrEmpty(value))
+            throw new DomainException($"{nameof(OrderName)} cannot be empty");
+        return new OrderName(value);
+    }
 }
 
 public record OrderId
 {
     public Guid Value { get; set; }
+    private OrderId(Guid value) => Value = value;
+    public static OrderId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+            throw new DomainException($"{nameof(OrderId)} cannot be empty");
+        return new OrderId(value);
+    }
 }
 
 public record ProductId
 {
     public Guid Value { get; set; }
+    private ProductId(Guid value) => Value = value;
+    public static ProductId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+            throw new DomainException($"{nameof(ProductId)} cannot be empty");
+        return new ProductId(value);
+    }
 }
 public record OrderItemId
 {
     public Guid Value { get; set; }
+    private OrderItemId(Guid value) => Value = value;
+    public static OrderItemId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+            throw new DomainException($"{nameof(OrderItemId)} cannot be empty");
+        return new OrderItemId(value);
+    }
 }
