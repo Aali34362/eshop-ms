109845becd3967edd076c3e5a85122c862c4338a
Aamir Ali <aa882755@gmail.com>
2024-06-23 23:39:48 +0530

    Migrations

--- Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
+++ Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -6,136 +6,111 @@ public class OrderConfiguration : IEntityTypeConfiguration<Order>
 {
     public void Configure(EntityTypeBuilder<Order> builder)
     {
-        builder.HasKey(c => c.Id);
-        builder.Property(c => c.Id)
-            .HasConversion(
-            orderId => orderId.Value,
-            dbId => OrderId.Of(dbId));
+        builder.HasKey(o => o.Id);
+
+        builder.Property(o => o.Id).HasConversion(
+                        orderId => orderId.Value,
+                        dbId => OrderId.Of(dbId));
 
         builder.HasOne<Customer>()
-            .WithMany()
-            .HasForeignKey(oi => oi.CustomerId);
+          .WithMany()
+          .HasForeignKey(o => o.CustomerId)
+          .IsRequired();
 
-        builder.HasMany<OrderItem>()
+        builder.HasMany(o => o.OrderItems)
             .WithOne()
             .HasForeignKey(oi => oi.OrderId);
-
         builder.ComplexProperty(
-            o => o.OrderName, nameBuilder =>
+        o => o.OrderName, nameBuilder =>
             {
                 nameBuilder.Property(n => n.Value)
-                .HasColumnName(nameof(Order.OrderName))
-                .HasMaxLength(100)
-                .IsRequired();
+                    .HasColumnName(nameof(Order.OrderName))
+                    .HasMaxLength(100)
+                    .IsRequired();
             });
 
         builder.ComplexProperty(
-            o => o.ShippingAddress, addressBuilder =>
-            {
-                addressBuilder.Property(n => n.FirstName)
-                .HasColumnName(nameof(Address.FirstName))
-                .HasMaxLength(50)
-                .IsRequired();
+           o => o.ShippingAddress, addressBuilder =>
+           {
+               addressBuilder.Property(a => a.FirstName)
+                   .HasMaxLength(50)
+                   .IsRequired();
 
-                addressBuilder.Property(n => n.LastName)
-                .HasColumnName(nameof(Address.LastName))
-                .HasMaxLength(50)
-                .IsRequired();
+               addressBuilder.Property(a => a.LastName)
+                                   .HasMaxLength(50)
+                                   .IsRequired();
 
-                addressBuilder.Property(n => n.EmailAddress)
-                .HasColumnName(nameof(Address.EmailAddress))
-                .HasMaxLength(50);
+               addressBuilder.Property(a => a.EmailAddress)
+                                  .HasMaxLength(50);
 
-                addressBuilder.Property(n => n.EmailAddress)
-                .HasColumnName(nameof(Address.EmailAddress))
-                .HasMaxLength(50);
+               addressBuilder.Property(a => a.AddressLine)
+                                  .HasMaxLength(180)
+                                  .IsRequired();
 
-                addressBuilder.Property(n => n.AddressLine)
-                .HasColumnName(nameof(Address.AddressLine))
-                .HasMaxLength(100).IsRequired();
+               addressBuilder.Property(a => a.Country)
+                                  .HasMaxLength(50);
 
-                addressBuilder.Property(n => n.Country)
-                .HasColumnName(nameof(Address.Country))
-                .HasMaxLength(50);
+               addressBuilder.Property(a => a.State)
+                                  .HasMaxLength(50);
 
-                addressBuilder.Property(n => n.State)
-                .HasColumnName(nameof(Address.State))
-                .HasMaxLength(50);
-
-                addressBuilder.Property(n => n.ZipCode)
-                .HasColumnName(nameof(Address.ZipCode))
-                .HasMaxLength(5);
-            });
+               addressBuilder.Property(a => a.ZipCode)
+                                  .HasMaxLength(5)
+                                  .IsRequired();
+           });
 
         builder.ComplexProperty(
-            o => o.BillingAddress, addressBuilder =>
-            {
-                addressBuilder.Property(n => n.FirstName)
-                .HasColumnName(nameof(Address.FirstName))
-                .HasMaxLength(50)
-                .IsRequired();
+          o => o.BillingAddress, addressBuilder =>
+          {
+              addressBuilder.Property(a => a.FirstName)
+                   .HasMaxLength(50)
+                   .IsRequired();
 
-                addressBuilder.Property(n => n.LastName)
-                .HasColumnName(nameof(Address.LastName))
-                .HasMaxLength(50)
-                .IsRequired();
+              addressBuilder.Property(a => a.LastName)
+                   .HasMaxLength(50)
+                   .IsRequired();
 
-                addressBuilder.Property(n => n.EmailAddress)
-                .HasColumnName(nameof(Address.EmailAddress))
-                .HasMaxLength(50);
+              addressBuilder.Property(a => a.EmailAddress)
+                  .HasMaxLength(50);
 
-                addressBuilder.Property(n => n.EmailAddress)
-                .HasColumnName(nameof(Address.EmailAddress))
-                .HasMaxLength(50);
+              addressBuilder.Property(a => a.AddressLine)
+                  .HasMaxLength(180)
+                  .IsRequired();
 
-                addressBuilder.Property(n => n.AddressLine)
-                .HasColumnName(nameof(Address.AddressLine))
-                .HasMaxLength(100).IsRequired();
+              addressBuilder.Property(a => a.Country)
+                  .HasMaxLength(50);
 
-                addressBuilder.Property(n => n.Country)
-                .HasColumnName(nameof(Address.Country))
-                .HasMaxLength(50);
+              addressBuilder.Property(a => a.State)
+                  .HasMaxLength(50);
 
-                addressBuilder.Property(n => n.State)
-                .HasColumnName(nameof(Address.State))
-                .HasMaxLength(50);
-
-                addressBuilder.Property(n => n.ZipCode)
-                .HasColumnName(nameof(Address.ZipCode))
-                .HasMaxLength(5);
-            });
+              addressBuilder.Property(a => a.ZipCode)
+                  .HasMaxLength(5)
+                  .IsRequired();
+          });
 
         builder.ComplexProperty(
-            o => o.Payment, paymentBuilder =>
-            {
-                paymentBuilder.Property(n => n.CardName)
-                .HasColumnName(nameof(Payment.CardName))
-                .HasMaxLength(50);
+               o => o.Payment, paymentBuilder =>
+               {
+                   paymentBuilder.Property(p => p.CardName)
+                       .HasMaxLength(50);
 
-                paymentBuilder.Property(n => n.CardNumber)
-               .HasColumnName(nameof(Payment.CardNumber))
-               .HasMaxLength(24)
-               .IsRequired();
+                   paymentBuilder.Property(p => p.CardNumber)
+                       .HasMaxLength(24)
+                       .IsRequired();
 
-                paymentBuilder.Property(n => n.CardNumber)
-              .HasColumnName(nameof(Payment.CardNumber))
-              .HasMaxLength(24)
-              .IsRequired();
+                   paymentBuilder.Property(p => p.Expiration)
+                       .HasMaxLength(10);
 
-                paymentBuilder.Property(n => n.Expiration)
-              .HasColumnName(nameof(Payment.Expiration))
-              .HasMaxLength(10);
+                   paymentBuilder.Property(p => p.CVV)
+                       .HasMaxLength(3);
 
-                paymentBuilder.Property(n => n.CVV)
-              .HasColumnName(nameof(Payment.CVV))
-              .HasMaxLength(3);
-            });
+                   paymentBuilder.Property(p => p.PaymentMethod);
+               });
 
-        builder.Property(c => c.Status)
+        builder.Property(o => o.Status)
             .HasDefaultValue(OrderStatus.Draft)
             .HasConversion(
-            s => s.ToString(),
-            dbStatus => (OrderStatus)Enum.Parse(typeof(OrderStatus),dbStatus));
+                s => s.ToString(),
+                dbStatus => (OrderStatus)Enum.Parse(typeof(OrderStatus), dbStatus));
 
         builder.Property(o => o.TotalPrice);
     }
